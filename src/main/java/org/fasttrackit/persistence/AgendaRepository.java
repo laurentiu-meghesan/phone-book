50891ae6df24e430ab0ff002378bf109055c3c8c
laurentiu-meghesan <laurentiu.meghesan@gmail.com>
2020-01-25 20:01:10 +0200

    added search contacts method

--- src/main/java/org/fasttrackit/persistence/AgendaRepository.java
+++ src/main/java/org/fasttrackit/persistence/AgendaRepository.java
@@ -2,6 +2,7 @@ package org.fasttrackit.persistence;
 
 import org.fasttrackit.domain.Agenda;
 import org.fasttrackit.transfer.CreateAgendaRequest;
+import org.fasttrackit.transfer.SearchAgendaRequest;
 import org.fasttrackit.transfer.UpdateAgendaRequest;
 
 import java.io.IOException;
@@ -74,18 +75,41 @@ public class AgendaRepository {
 
             List<Agenda> contacts = new ArrayList<>();
 
-            while (resultSet.next()){
-                 Agenda agenda = new Agenda();
-                 agenda.setId(resultSet.getLong("id"));
-                 agenda.setFirst_name(resultSet.getString("first_name"));
-                 agenda.setLast_name(resultSet.getString("last_name"));
-                 agenda.setPhone_number(resultSet.getString("phone_number"));
+            while (resultSet.next()) {
+                Agenda agenda = new Agenda();
+                agenda.setId(resultSet.getLong("id"));
+                agenda.setFirst_name(resultSet.getString("first_name"));
+                agenda.setLast_name(resultSet.getString("last_name"));
+                agenda.setPhone_number(resultSet.getString("phone_number"));
 
-                 contacts.add(agenda);
+                contacts.add(agenda);
             }
             return contacts;
         }
-
     }
 
+    public List<Agenda> searchContact(SearchAgendaRequest request) throws IOException, SQLException {
+        String sql = "SELECT id,first_name,last_name,phone_number FROM agenda WHERE first_name LIKE \"%?%\"" +
+                " OR last_name LIKE \"%?%\"";
+        try (Connection connection = DatabaseConfiguration.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            preparedStatement.setString(1, request.getPattern());
+
+            ResultSet resultSet = preparedStatement.executeQuery(sql);
+
+            List<Agenda> contacts1 = new ArrayList<>();
+
+            while (resultSet.next()) {
+                Agenda agenda = new Agenda();
+                agenda.setId(resultSet.getLong("id"));
+                agenda.setFirst_name(resultSet.getString("first_name"));
+                agenda.setLast_name(resultSet.getString("last_name"));
+                agenda.setPhone_number(resultSet.getString("phone_number"));
+
+                contacts1.add(agenda);
+            }
+            return contacts1;
+        }
+    }
 }
