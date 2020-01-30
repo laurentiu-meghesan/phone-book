9fc5e0370a20242f8bf78c9fa274a8591faaee15
laurentiu-meghesan <laurentiu.meghesan@gmail.com>
2020-01-30 22:51:01 +0200

    added methods doPut doGet....

--- src/main/java/org/fasttrackit/web/AgendaServlet.java
+++ src/main/java/org/fasttrackit/web/AgendaServlet.java
@@ -1,8 +1,10 @@
 package org.fasttrackit.web;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.fasttrackit.domain.Agenda;
 import org.fasttrackit.service.AgendaService;
 import org.fasttrackit.transfer.CreateContactRequest;
+import org.fasttrackit.transfer.UpdateContactRequest;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 @WebServlet("/agenda")
 public class AgendaServlet extends HttpServlet {
@@ -24,6 +27,44 @@ public class AgendaServlet extends HttpServlet {
 
         try {
             agendaService.createContact(request);
+        } catch (SQLException | ClassNotFoundException e) {
+            resp.sendError(500, "Internal server error: " + e.getMessage());
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String id = req.getParameter("id");
+        try {
+            agendaService.deleteContact(Long.parseLong(id));
+        } catch (SQLException | ClassNotFoundException e) {
+            resp.sendError(500, "Internal server error: " + e.getMessage());
+        }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String id = req.getParameter("id");
+        UpdateContactRequest request =
+                new ObjectMapper().readValue(req.getReader(), UpdateContactRequest.class);
+
+        try {
+            agendaService.updateContact(Long.parseLong(id), request);
+        } catch (SQLException | ClassNotFoundException e) {
+            resp.sendError(500, "Internal server error: " + e.getMessage());
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        try {
+            List<Agenda> contactsList = agendaService.getContacts();
+
+            String response = new ObjectMapper().writeValueAsString(contactsList);
+            resp.getWriter().print(response);
         } catch (SQLException | ClassNotFoundException e) {
             resp.sendError(500, "Internal server error: " + e.getMessage());
         }
